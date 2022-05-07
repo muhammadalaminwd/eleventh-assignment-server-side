7019a6a674b4f5a0496be357c183ab2174922824
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-07 16:30:52 +0600

    delete button created

--- index.js
+++ index.js
@@ -34,13 +34,19 @@ async function run(){
         });
 
             // POST
-
     app.post('/service', async(req, res) => {
         const newService = req.body;
         const result = await serviceCollection.insertOne(newService);
         res.send(result);
     })
 
+    // DELETE
+    app.delete('/service/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await serviceCollection.deleteOne(query);
+        res.send(result)
+    });
 
     }
     finally{
