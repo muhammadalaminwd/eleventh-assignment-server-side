613d64444d07a518601daf2f903511a1d6e1a254
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-07 22:10:01 +0600

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,29 +24,12 @@ async function run(){
             res.send(products);
         });
 
-        app.get('/product/:id', async(req, res) => {
-            const id = req.id;
-            console.log(id)
-            const query = {};
-            const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products);
+        app.get('/product/:id' , async (req , res )=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const service = await productCollection.findOne(query);
+            res.send(service);
         });
-
-            // POST
-    app.post('/service', async(req, res) => {
-        const newService = req.body;
-        const result = await serviceCollection.insertOne(newService);
-        res.send(result);
-    })
-
-    // DELETE
-    app.delete('/service/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: ObjectId(id)};
-        const result = await serviceCollection.deleteOne(query);
-        res.send(result)
-    });
 
     }
     finally{
